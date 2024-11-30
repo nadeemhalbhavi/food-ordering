e847272da1759d9047e9bfb9cf8893f471952437
nadeemhalbhavi <nadeem.halbhavi@gmail.com>
2024-12-01 01:47:27 +0530

    cart remove quantity functionality is working fine

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,19 +2,43 @@ import React from "react";
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
 import CartContext from "../../store/Cart-Context";
-import Card from "../UI/Card";
+
+import CartItem from "./CartItem";
 
 const Cart = (props) => {
   const cartCtx = React.useContext(CartContext);
+  let totalPrice = cartCtx.items.reduce((acc, cv) => {
+    return acc + cv.price * cv.quantity;
+  }, 0);
+
+  const hasItem = cartCtx.items.length > 0;
+
+  const RemoveItemHandler = (id) => {
+    console.log("Removing item", id);
+    cartCtx.removeItem(id);
+  };
+
+  const AddItemHandler = (id) => {
+    console.log("Adding item", id);
+    cartCtx.addItem(id);
+    // cartCtx.addItem({
+    //   ...cartCtx.items.find((item) => item.id === id),
+    //   quantity: cartCtx.items.find((item) => item.id === id).quantity + 1,
+    // });
+  };
 
   const cartItem = (
     <ul className={classes.cartItems}>
       {cartCtx.items.map((item) => (
-        <Card>
-          <li>{item.name}</li>
-          <li>{item.description}</li>
-          <li>{item.price}</li>
-        </Card>
+        <CartItem
+          name={item.name}
+          summary={item.description}
+          price={item.price}
+          quantity={item.quantity}
+          key={item.id}
+          onRemove={() => RemoveItemHandler(item.id)}
+          onAdd={() => AddItemHandler(item.id)}
+        />
       ))}
     </ul>
   );
@@ -23,13 +47,13 @@ const Cart = (props) => {
       {cartItem}
       <div className={classes.total} onClick={props.onHideCart}>
         <span>Total Amount</span>
-        <span>49.99</span>
+        <span>{totalPrice.toFixed(2)}</span>
       </div>
       <div className={classes.actions}>
         <button onClick={props.onHideCart} className={classes.buttonAlt}>
           Close
         </button>
-        <button className={classes.button}>Order</button>
+        {hasItem && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -3,17 +3,37 @@ import CartContext from "./Cart-Context";
 
 const CartProvider = (props) => {
   const [item, setUpdateItems] = useState([]);
+
   const addItemToCartHandler = (item) => {
     setUpdateItems((prevState) => [...prevState, item]);
   };
 
-  const removeItemFromCartHandler = (id) => {};
+  const removeItemFromCartHandler = (id) => {
+    setUpdateItems((prevItems) => {
+      const existingItemIndex = prevItems.findIndex((item) => item.id === id);
+
+      const existingItem = prevItems[existingItemIndex];
+
+      if (existingItem.quantity === 1) {
+        // Remove item if quantity is 1
+        return prevItems.filter((item) => item.id !== id);
+      } else {
+        // Decrease quantity if more than 1
+        const updatedItem = {
+          ...existingItem,
+          quantity: existingItem.quantity - 1,
+        };
+        const updatedItems = [...prevItems];
+        updatedItems[existingItemIndex] = updatedItem;
+        return updatedItems;
+      }
+    });
+  };
   const cartcontext = {
     items: item,
     totalAmount: 0,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
-    don: "I am DON",
   };
   return (
     <CartContext.Provider value={cartcontext}>
